ff93917c2dcaef8737040e9e7d7af6ac28ff1ae1
Johnny <jonathanoreilly@live.co.uk>
2023-07-20 10:17:08 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from '../src/components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <h1>this is a heading</h1> */}
+      <Header />
     </div>
   );
 }
